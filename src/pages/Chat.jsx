d8c57b7aee93aa13072c2bbe1c91a55a903aef0a
Romulo <romulotg12@gmail.com>
2025-06-07 17:40:00 -0300

    feat: importing and using desktop on chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,11 +1,12 @@
 import './styles.css'
 import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
+import Desktop from '../layout/Desktop';
 
 export default function Chat() {
     const { t } = useTranslation('home');
 
     return (
-        <div>Oi!</div>
+        <Desktop />
     )
 };
