ec7781b0c24d2637e7ef1a0e366aa61e2c77483c
Romulo <romulotg12@gmail.com>
2025-06-09 21:42:47 -0300

    feat: adding translation in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
+import { useTranslation } from "react-i18next";
 import LanguageSelector from "./LanguageSelector";
 
 export default function Footer() {
+    const { t } = useTranslation("footer");
     return (
         <div className="flex justify-between bg-gradient-to-b from-blue-50 to-blue-100 rounded-b-lg px-4 py-2 border-t border-blue-200 ">
             <div className="flex justify-center items-center gap-2 text-xs text-gray-600">
                 <a href="#" className="hover:underline">
-                    Declaração de privacidade
+                    {t('privacy')}
                 </a>
                 |
                 <a href="#" className="hover:underline">
-                    Termos de uso
+                    {t('usage')}
                 </a>
                 |
                 <a href="#" className="hover:underline">
-                    Status do serviço
+                    {t('status')}
                 </a>
             </div>
 
