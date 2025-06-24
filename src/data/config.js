2def3b7b22a24466cc1eaf9d9e248bde01516fe6
romulodm <romulotg12@gmail.com>
2025-06-23 23:22:39 -0300

    refact: changing the way of getting the token to configure axios

--- src/data/config.js
+++ src/data/config.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import secureLocalStorage from "react-secure-storage";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -6,19 +7,22 @@ export const requestConfig = axios.create({
   baseURL: BASE_URL,
 });
 
-/*
-apiRequest.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-  const currentUser = user && JSON.parse(user).currentUser;
-  const TOKEN = currentUser?.accessToken;
+requestConfig.interceptors.request.use((config) => {
+  const tokenData = secureLocalStorage.getItem('flm-token');
 
-  if (TOKEN) {
-    config.headers.token = `Bearer ${TOKEN}`;
-    config.headers.user = user;
+  if (tokenData && typeof tokenData === 'object' && tokenData.value && tokenData.expiresAt) {
+    const now = new Date();
+    const expiresAt = new Date(tokenData.expiresAt);
+
+    if (now < expiresAt) {
+      config.headers.Authorization = `Bearer ${tokenData.value}`;
+    } else {
+      secureLocalStorage.removeItem('flm-token');
+      secureLocalStorage.removeItem('flm-user');
+    }
   }
 
   return config;
 });
-*/
 
 export default requestConfig;
