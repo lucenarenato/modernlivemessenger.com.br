009f8785e45a9f7a78d7afd56f08bae966f30ea9
romulodm <romulotg12@gmail.com>
2025-06-25 21:32:00 -0300

    feat: adding contexts (providers) in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,11 @@ import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
 
 import { PortfolioThemeProvider } from './context/ThemeContext.jsx';
+import { EmoticonProvider } from './context/EmoticonContext.jsx';
+import { WindowManagerProvider } from './context/WindowManagerContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ToastProvider } from './context/ToastContext.jsx';
+import { ChatProvider } from './context/ChatContext.jsx';
 import { ThemeProvider } from '@mui/system';
 import { theme } from './theme/theme.js';
 
@@ -20,9 +23,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <ToastProvider>
         <PortfolioThemeProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
+          <WindowManagerProvider>
+            <AuthProvider>
+              <ChatProvider>
+                <EmoticonProvider>
+                  <App />
+                </EmoticonProvider>
+              </ChatProvider>
+            </AuthProvider>
+          </WindowManagerProvider>
         </PortfolioThemeProvider>
       </ToastProvider>
     </ThemeProvider>
