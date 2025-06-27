804b6e064062dc152d4c0ece1aa8e8b79e94383b
romulodm <romulotg12@gmail.com>
2025-06-27 14:25:40 -0300

    feat: functions to import assets that are used dynamically by the application

---
+++ src/imports/winks.jsx
@@ -0,0 +1,67 @@
+import bouncy_ball from './assets/winks/bouncy_ball/bouncy_ball.swf?url';
+import bow from './assets/winks/bow/bow.swf?url';
+import dancing_pig from './assets/winks/dancing_pig/dancing_pig.swf?url';
+import eyeball from './assets/winks/eyeball/eyeball.swf?url';
+import frog from './assets/winks/frog/frog.swf?url';
+import guitar_smash from './assets/winks/guitar_smash/guitar_smash.swf?url';
+import heart from './assets/winks/heart/heart.swf?url';
+import kiss from './assets/winks/kiss/kiss.swf?url';
+import knock from './assets/winks/knock/knock.swf?url';
+import lipstick_girl from './assets/winks/lipstick_girl/lipstick_girl.swf?url';
+import love_letter from './assets/winks/love_letter/love_letter.swf?url';
+import notes from './assets/winks/notes/notes.swf?url';
+import silly_face from './assets/winks/silly_face/silly_face.swf?url';
+import stars from './assets/winks/stars/stars.swf?url';
+import water_balloon from './assets/winks/water_balloon/water_balloon.swf?url';
+
+import bouncy_ball_icon from './assets/winks/bouncy_ball/bouncy_ball.png';
+import bow_icon from './assets/winks/bow/bow.jpg';
+import dancing_pig_icon from './assets/winks/dancing_pig/dancing_pig.png';
+import eyeball_icon from './assets/winks/eyeball/eyeball.png';
+import frog_icon from './assets/winks/frog/frog.png';
+import guitar_smash_icon from './assets/winks/guitar_smash/guitar_smash.png';
+import heart_icon from './assets/winks/heart/heart.png';
+import kiss_icon from './assets/winks/kiss/kiss.png';
+import knock_icon from './assets/winks/knock/knock.png';
+import lipstick_girl_icon from './assets/winks/lipstick_girl/lipstick_girl.png';
+import love_letter_icon from './assets/winks/love_letter/love_letter.png';
+import notes_icon from './assets/winks/notes/notes.png';
+import silly_face_icon from './assets/winks/silly_face/silly_face.png';
+import stars_icon from './assets/winks/stars/stars.png';
+import water_balloon_icon from './assets/winks/water_balloon/water_balloon.png';
+
+export const winks = {
+  bouncy_ball: { path: bouncy_ball, duration: 6.5 },
+  bow: { path: bow, duration: 10.5 },
+  dancing_pig: { path: dancing_pig, duration: 11.5 },
+  eyeball: { path: eyeball, duration: 6 },
+  frog: { path: frog, duration: 10 },
+  guitar_smash: { path: guitar_smash, duration: 6 },
+  heart: { path: heart, duration: 6.6 },
+  kiss: { path: kiss, duration: 3 },
+  knock: { path: knock, duration: 2.5 },
+  lipstick_girl: { path: lipstick_girl, duration: 9.5 },
+  love_letter: { path: love_letter, duration: 8 },
+  notes: { path: notes, duration: 9 },
+  silly_face: { path: silly_face, duration: 7 },
+  stars: { path: stars, duration: 5 },
+  water_balloon: { path: water_balloon, duration: 8 },
+};
+
+export const winks_icons = {
+  bouncy_ball_icon,
+  bow_icon,
+  dancing_pig_icon,
+  eyeball_icon,
+  frog_icon,
+  guitar_smash_icon,
+  heart_icon,
+  kiss_icon,
+  knock_icon,
+  lipstick_girl_icon,
+  love_letter_icon,
+  notes_icon,
+  silly_face_icon,
+  stars_icon,
+  water_balloon_icon,
+};
